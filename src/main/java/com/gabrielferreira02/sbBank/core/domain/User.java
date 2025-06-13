a7e353f63d6cb9424b55f47cf0311f1ec50ca1e1
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-06-13 20:36:23 -0300

    feat(domain): Add User entity

---
+++ src/main/java/com/gabrielferreira02/sbBank/core/domain/User.java
@@ -0,0 +1,18 @@
+package com.gabrielferreira02.sbBank.core.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record User(UUID id,
+                   String name,
+                   String cpf,
+                   String email,
+                   String password,
+                   boolean verified,
+                   String cep,
+                   String cepComplement,
+                   String phone,
+                   LocalDateTime createdAt,
+                   Account account
+                   ) {
+}
