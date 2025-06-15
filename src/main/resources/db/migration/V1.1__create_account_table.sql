a9d1be061fd9b37a60db8146aeef807df13daa86
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-06-15 19:47:04 -0300

    chore(database): update fields in create_account_table migration - Added check at balance field

--- src/main/resources/db/migration/V1.1__create_account_table.sql
+++ src/main/resources/db/migration/V1.1__create_account_table.sql
@@ -1,13 +1,13 @@
 CREATE TABLE public."account" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL UNIQUE CONSTRAINTS fk_account_user REFERENCES public.user(id),
+    user_id UUID NOT NULL UNIQUE CONSTRAINT fk_account_user REFERENCES public.user(id),
     account_number VARCHAR(30) NOT NULL UNIQUE,
-    balance DECIMAL(19, 4) NOT NULL,
+    balance DECIMAL(19, 4) NOT NULL CHECK(balance >= 0),
     daily_transfer_limit DECIMAL(19, 4) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT positive_balance CHECK (balance >= 0),
     CONSTRAINT positive_limit CHECK (daily_transfer_limit >= 0)
-)
+);
 
 CREATE INDEX idx_account_user_id ON public.account(user_id);
 CREATE INDEX idx_account_number ON public.account(account_number);
