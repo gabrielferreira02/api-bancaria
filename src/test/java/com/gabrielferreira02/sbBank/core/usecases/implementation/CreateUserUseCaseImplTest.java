5c140f3bbeb2dce0b4fc328e00a223cb3fe11a12
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-06-19 19:52:45 -0300

    feat(user): create user usecase interface and implementations

---
+++ src/test/java/com/gabrielferreira02/sbBank/core/usecases/implementation/CreateUserUseCaseImplTest.java
@@ -0,0 +1,272 @@
+package com.gabrielferreira02.sbBank.core.usecases.implementation;
+
+import com.gabrielferreira02.sbBank.core.domain.User;
+import com.gabrielferreira02.sbBank.core.exceptions.InvalidCepException;
+import com.gabrielferreira02.sbBank.core.exceptions.InvalidCpfException;
+import com.gabrielferreira02.sbBank.core.gateways.UserGateway;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class CreateUserUseCaseImplTest {
+
+    @Mock
+    private UserGateway userGateway;
+    @InjectMocks
+    private CreateUserUseCaseImpl createUserUseCase;
+
+    @Test
+    @DisplayName("Usuario criado com sucesso")
+    void success() {
+        User userInput = new User(
+                null,
+                "User",
+                "17824829707",
+                "user@email.com",
+                "12345678",
+                false,
+                "22770-640",
+                "131 bl 5 ap 404",
+                "5521976027679",
+                null
+        );
+
+        User userOutput = new User(
+                UUID.randomUUID(),
+                userInput.name(),
+                userInput.cpf(),
+                userInput.email(),
+                userInput.password(),
+                userInput.verified(),
+                userInput.cep(),
+                userInput.cepComplement(),
+                userInput.phone(),
+                LocalDateTime.now()
+        );
+
+        doReturn(true).when(userGateway).validateCpf(userInput.cpf());
+        doReturn(true).when(userGateway).validateCep(userInput.cep());
+        doReturn(null).when(userGateway).findByCpf(userInput.cpf());
+        doReturn(userOutput).when(userGateway).createUser(any(User.class));
+
+        User response = createUserUseCase.execute(userInput);
+
+        assertNotNull(response);
+        assertNotNull(response.id());
+        assertEquals(userInput.name(), response.name());
+        assertEquals(userInput.cpf(), response.cpf());
+        assertEquals(userInput.email(), response.email());
+        assertEquals(userInput.password(), response.password());
+        assertFalse(response.verified());
+        assertEquals(userInput.cep(), response.cep());
+        assertEquals(userInput.cepComplement(), response.cepComplement());
+        assertEquals(userInput.phone(), response.phone());
+
+    }
+
+    @Test
+    @DisplayName("Falha ao criar usuario devido a cpf invalido")
+    void error1() {
+        User userInput = new User(
+                null,
+                "User",
+                "11111111111",
+                "user@email.com",
+                "12345678",
+                false,
+                "22770-640",
+                "131 bl 5 ap 404",
+                "5521976027679",
+                null
+        );
+
+        doReturn(true).when(userGateway).validateCep(userInput.cep());
+        doReturn(false).when(userGateway).validateCpf(userInput.cpf());
+
+        assertThrows(InvalidCpfException.class, () -> {
+           createUserUseCase.execute(userInput);
+        });
+    }
+
+    @Test
+    @DisplayName("Falha ao criar usuario devido a cep invalido")
+    void error2() {
+        User userInput = new User(
+                null,
+                "User",
+                "17824829707",
+                "user@email.com",
+                "12345678",
+                false,
+                "1111-111",
+                "131 bl 5 ap 404",
+                "5521976027679",
+                null
+        );
+
+        doReturn(false).when(userGateway).validateCep(userInput.cep());
+
+        assertThrows(InvalidCepException.class, () -> {
+            createUserUseCase.execute(userInput);
+        });
+    }
+
+    @Test
+    @DisplayName("Falha ao criar usuario devido a 'name' invalido")
+    void error3() {
+        User userInput = new User(
+                null,
+                "",
+                "17824829707",
+                "user@email.com",
+                "12345678",
+                false,
+                "22770-640",
+                "131 bl 5 ap 404",
+                "5521976027679",
+                null
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            createUserUseCase.execute(userInput);
+        });
+    }
+
+    @Test
+    @DisplayName("Falha ao criar usuario devido a 'email' invalido")
+    void error4() {
+        User userInput = new User(
+                null,
+                "User",
+                "17824829707",
+                "",
+                "12345678",
+                false,
+                "22770-640",
+                "131 bl 5 ap 404",
+                "5521976027679",
+                null
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            createUserUseCase.execute(userInput);
+        });
+    }
+
+    @Test
+    @DisplayName("Falha ao criar usuario devido a campo cep em branco")
+    void error5() {
+        User userInput = new User(
+                null,
+                "User",
+                "17824829707",
+                "user@email.com",
+                "12345678",
+                false,
+                "",
+                "131 bl 5 ap 404",
+                "5521976027679",
+                null
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            createUserUseCase.execute(userInput);
+        });
+    }
+
+    @Test
+    @DisplayName("Falha ao criar usuario devido a telefone em branco")
+    void error6() {
+        User userInput = new User(
+                null,
+                "User",
+                "17824829707",
+                "user@email.com",
+                "12345678",
+                false,
+                "22770-640",
+                "131 bl 5 ap 404",
+                "",
+                null
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            createUserUseCase.execute(userInput);
+        });
+    }
+
+    @Test
+    @DisplayName("Falha ao criar usuario devido a complemento do cep em branco")
+    void error7() {
+        User userInput = new User(
+                null,
+                "User",
+                "17824829707",
+                "user@email.com",
+                "12345678",
+                false,
+                "22770-640",
+                "",
+                "5521976027679",
+                null
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            createUserUseCase.execute(userInput);
+        });
+    }
+
+    @Test
+    @DisplayName("Falha ao criar usuario devido a telefone com menos de 8 caracteres")
+    void error8() {
+        User userInput = new User(
+                null,
+                "User",
+                "17824829707",
+                "user@email.com",
+                "12345678",
+                false,
+                "22770-640",
+                "131 bl 5 ap 404",
+                "7602767",
+                null
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            createUserUseCase.execute(userInput);
+        });
+    }
+
+    @Test
+    @DisplayName("Falha ao criar usuario devido a senha curta demais")
+    void error9() {
+        User userInput = new User(
+                null,
+                "User",
+                "17824829707",
+                "user@email.com",
+                "12345",
+                false,
+                "22770-640",
+                "131 bl 5 ap 404",
+                "5521976027679",
+                null
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            createUserUseCase.execute(userInput);
+        });
+    }
+
+}
