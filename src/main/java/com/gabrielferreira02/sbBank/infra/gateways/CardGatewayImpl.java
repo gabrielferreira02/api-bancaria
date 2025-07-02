49ca9c746733ec65e8353934284cce050b3af448
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-07-02 19:15:16 -0300

    feat(gateway): create card gateway implementation and remove generate card number method from interface

---
+++ src/main/java/com/gabrielferreira02/sbBank/infra/gateways/CardGatewayImpl.java
@@ -0,0 +1,16 @@
+package com.gabrielferreira02.sbBank.infra.gateways;
+
+import com.gabrielferreira02.sbBank.core.domain.BankingConstants;
+import com.gabrielferreira02.sbBank.core.domain.Card;
+import com.gabrielferreira02.sbBank.core.gateways.CardGateway;
+import com.gabrielferreira02.sbBank.core.usecases.interfaces.CreateCardNumberUseCase;
+
+import java.util.Random;
+
+public class CardGatewayImpl implements CardGateway {
+
+    @Override
+    public Card createCard(Card card) {
+        return null;
+    }
+}
