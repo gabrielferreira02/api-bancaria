9a5935b9fb35be98e080dbec7885522814991fe1
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-07-02 18:48:54 -0300

    refactor(use case): replaces the old gateway method call with the new use case call

--- src/main/java/com/gabrielferreira02/sbBank/core/usecases/implementation/CreateAccountUseCaseImpl.java
+++ src/main/java/com/gabrielferreira02/sbBank/core/usecases/implementation/CreateAccountUseCaseImpl.java
@@ -3,6 +3,7 @@ package com.gabrielferreira02.sbBank.core.usecases.implementation;
 import com.gabrielferreira02.sbBank.core.domain.Account;
 import com.gabrielferreira02.sbBank.core.domain.User;
 import com.gabrielferreira02.sbBank.core.gateways.AccountGateway;
+import com.gabrielferreira02.sbBank.core.usecases.interfaces.CreateAccountNumberUseCase;
 import com.gabrielferreira02.sbBank.core.usecases.interfaces.CreateAccountUseCase;
 
 import java.math.BigDecimal;
@@ -11,9 +12,11 @@ import java.time.LocalDateTime;
 public class CreateAccountUseCaseImpl implements CreateAccountUseCase {
 
     private final AccountGateway accountGateway;
+    private final CreateAccountNumberUseCase createAccountNumberUseCase;
 
-    public CreateAccountUseCaseImpl(AccountGateway accountGateway) {
+    public CreateAccountUseCaseImpl(AccountGateway accountGateway, CreateAccountNumberUseCase createAccountNumberUseCase) {
         this.accountGateway = accountGateway;
+        this.createAccountNumberUseCase = createAccountNumberUseCase;
     }
 
     @Override
@@ -23,7 +26,7 @@ public class CreateAccountUseCaseImpl implements CreateAccountUseCase {
         Account newAccount = new Account(
                 null,
                 user,
-                accountGateway.generateAccountNumber(),
+                createAccountNumberUseCase.execute(),
                 new BigDecimal(0),
                 new BigDecimal(15000),
                 LocalDateTime.now()
