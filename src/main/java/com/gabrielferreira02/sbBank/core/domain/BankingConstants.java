51497901930daf3da989409a5399e3e120a388fb
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-07-02 19:12:24 -0300

    refactor(domain): add bin,banking code and card number length in BankingConstants

--- src/main/java/com/gabrielferreira02/sbBank/core/domain/BankingConstants.java
+++ src/main/java/com/gabrielferreira02/sbBank/core/domain/BankingConstants.java
@@ -7,7 +7,9 @@ public final class BankingConstants {
     public static final double SAVINGS_ACCOUNT_INTEREST_RATE = 0.005;
     public static final List<Integer> DAYS_ALLOWED_FOR_INVOICE_PAYMENT = List.of(6, 12, 19, 26);
     public static final BigDecimal DAILY_TRANSFER_LIMIT = new BigDecimal(15000);
-    public static final String bankingCode = "999";
+    public static final String BANKING_CODE = "999";
+    public static final String BIN = "0211";
+    public static final int CARD_LENGTH = 16;
 
     public BankingConstants() {}
 }
